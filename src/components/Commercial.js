ae4909ac7e861435604f16db66c0d265b69e09d5
bbennett7 <bryn.bennett@gmail.com>
2019-08-09 12:08:48 -0700

    Create single page components

---
+++ src/components/Commercial.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Commercial extends Component {
+  render() {
+    return(
+      <p>Commercial</p>
+    )
+  }
+}
---
+++ src/components/Residential.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Residential extends Component {
+  render() {
+    return(
+      <p>Residential</p>
+    )
+  }
+}
