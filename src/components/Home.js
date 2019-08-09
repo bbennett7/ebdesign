48d712713bc238e6bb80795c3d51c393dce75b19
bbennett7 <bryn.bennett@gmail.com>
2019-08-09 12:15:22 -0700

    Change routing so home component displays on / route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default Home extends Component {
+export default class Home extends Component {
   render() {
     return (
       <p>Home</p>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Render } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+
+import Home from './components/Home'
+import About from './components/About'
+import Residential from './components/Residential'
+import Commercial from './components/Commercial'
+import Contact from './components/Contact'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -10,12 +17,13 @@ ReactDOM.render(
     <React.Fragment>
       <Route path="/" component={App} />
       <Route exact path="/" component={Home} />
+      <Route exact path="/home" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/residential" component={Residential} />
       <Route exact path="/commercial" component={Commercial} />
       <Route exact path="/contact" component={Contact} />
     </React.Fragment>
-  </Router>
+  </Router>,
 document.getElementById('root'));
 
 serviceWorker.unregister();
