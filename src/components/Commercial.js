68b77a403c35546e4cabc8fed33df4f1cdc7b1f1
bbennett7 <bryn.bennett@gmail.com>
2019-08-09 15:56:25 -0700

    Add carousel to properties, need to make dynamic

--- src/components/Commercial.js
+++ src/components/Commercial.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Property from './Property'
 
+// Chadwick Images
+import LivingRoom1 from '../images/chadwick/LivingRoom1.jpg';
+import LivingRoom2 from '../images/chadwick/LivingRoom2.jpg';
+
 export default class Commercial extends Component {
+
+
   render() {
     return(
       <div className="Commercial">
-        <Property name="The Chadwick" />
+        <Property
+          name="The Chadwick"
+          firstImgSrc={LivingRoom1}
+          firstImgTitle="Living Room"/>
         <Property name="Aspen" />
       </div>
     )
--- src/components/Property.js
+++ src/components/Property.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 
 export default class Property extends Component {
   render() {
     return(
       <div className="Property">
         <h5>{this.props.name}</h5>
+        <Carousel className="Carousel">
+        <Carousel.Item>
+          <img
+            className="Carousel-image"
+            src={this.props.firstImgSrc}
+            alt="First slide of an apartment lobby"
+          />
+          <Carousel.Caption>
+            <p>{this.props.firstImgTitle}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="Carousel-image"
+            src={this.props.secondImgSrc}
+            alt="First slide of an apartment lobby"
+          />
+          <Carousel.Caption>
+            <p>{this.props.secondImgTitle}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        </Carousel>
       </div>
     )
   }
