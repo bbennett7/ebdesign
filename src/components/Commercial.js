28a78cb8581663ee8f38c4c47882c20f4cb4ad98
bbennett7 <bryn.bennett@gmail.com>
2019-08-09 16:48:37 -0700

    Create individual component for each property to customize carousel

--- src/components/Commercial.js
+++ src/components/Commercial.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import Property from './Property'
+import Chadwick from './properties/Chadwick'
 
-// Chadwick Images
-import LivingRoom1 from '../images/chadwick/LivingRoom1.jpg';
-import LivingRoom2 from '../images/chadwick/LivingRoom2.jpg';
 
 export default class Commercial extends Component {
 
@@ -11,11 +8,7 @@ export default class Commercial extends Component {
   render() {
     return(
       <div className="Commercial">
-        <Property
-          name="The Chadwick"
-          firstImgSrc={LivingRoom1}
-          firstImgTitle="Living Room"/>
-        <Property name="Aspen" />
+        <Chadwick />
       </div>
     )
   }
--- src/components/properties/Chadwick.js
+++ src/components/properties/Chadwick.js
@@ -2,31 +2,34 @@ import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 
-export default class Property extends Component {
+import LivingRoom1 from '../../images/chadwick/LivingRoom1.jpg';
+import LivingRoom2 from '../../images/chadwick/LivingRoom2.jpg';
+
+export default class Chadwick extends Component {
   render() {
     return(
       <div className="Property">
-        <h5>{this.props.name}</h5>
-        <Carousel className="Carousel">
+        <h5>The Chadwick</h5>
+        <Carousel interval="null" className="Carousel">
         <Carousel.Item>
           <img
             className="Carousel-image"
-            src={this.props.firstImgSrc}
-            alt="First slide of an apartment lobby"
+            src={LivingRoom1}
+            alt="First slide of a living room"
           />
           <Carousel.Caption>
-            <p>{this.props.firstImgTitle}</p>
+            <p>Living Room</p>
           </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item>
           <img
             className="Carousel-image"
-            src={this.props.secondImgSrc}
-            alt="First slide of an apartment lobby"
+            src={LivingRoom2}
+            alt="Second slide of living room"
           />
           <Carousel.Caption>
-            <p>{this.props.secondImgTitle}</p>
+            <p>Living room number 2</p>
           </Carousel.Caption>
         </Carousel.Item>
         </Carousel>
