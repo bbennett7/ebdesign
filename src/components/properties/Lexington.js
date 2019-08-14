3c7fefbcc90091618821a68d21d70aa2d29afc84
bbennett7 <bryn.bennett@gmail.com>
2019-08-14 12:04:23 -0700

    Change photos in Lexington component

--- src/components/properties/Lexington.js
+++ src/components/properties/Lexington.js
@@ -6,8 +6,8 @@ import BBQArea from '../../images/lexington/BBQ-area.jpg';
 import BBQs from '../../images/lexington/BBQs.jpg';
 import Entrance from '../../images/lexington/Entrance.jpg';
 import ExteriorBuilding from '../../images/lexington/Exterior-building.jpg';
-import LeasingExterior from '../../images/lexington/Leasing-exterior.jpg';
 import LeasingInterior from '../../images/lexington/Leasing-interior.jpg';
+import LeasingOffice from '../../images/lexington/Leasing-office.jpg';
 import MainExterior from '../../images/lexington/Main-exterior.jpg';
 import Pool from '../../images/lexington/Pool.jpg';
 
@@ -17,85 +17,85 @@ export default class Lexington extends Component {
       <div className="Property">
         <h4>The Lexington</h4>
         <Carousel interval="null" className="Carousel">
-        <Carousel.Item>
-          <img
-            className="Carousel-image"
-            src={BBQs}
-            alt=""
-          />
-          <Carousel.Caption>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={Entrance}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            className="Carousel-image"
-            src={Entrance}
-            alt=""
-          />
-          <Carousel.Caption>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={LeasingInterior}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            className="Carousel-image"
-            src={LeasingInterior}
-            alt=""
-          />
-          <Carousel.Caption>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={LeasingOffice}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            className="Carousel-image"
-            src={LeasingExterior}
-            alt=""
-          />
-          <Carousel.Caption>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={BBQs}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            className="Carousel-image"
-            src={BBQArea}
-            alt=""
-          />
-          <Carousel.Caption>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={BBQArea}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            className="Carousel-image"
-            src={Pool}
-            alt=""
-          />
-          <Carousel.Caption>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={Pool}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            className="Carousel-image"
-            src={MainExterior}
-            alt=""
-          />
-          <Carousel.Caption>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={MainExterior}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            className="Carousel-image"
-            src={ExteriorBuilding}
-            alt=""
-          />
-          <Carousel.Caption>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={ExteriorBuilding}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
         </Carousel>
       </div>
     )
