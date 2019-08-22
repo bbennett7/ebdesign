d36ebcc1272578bb9fcdcb268e9dca5de993ab6f
bbennett7 <bryn.bennett@gmail.com>
2019-08-22 13:21:55 -0700

    Build multi-family exterior component

--- src/components/properties/Multifamily-exterior.js
+++ src/components/properties/Multifamily-exterior.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 
+import P5Exterior from '../../images/parc5/Exterior.jpg';
+import LexEntrance from '../../images/lexington/Entrance.jpg';
+import LexExterior from '../../images/lexington/Exterior-building.jpg';
+import LexMainExterior from '../../images/lexington/Main-exterior.jpg';
+import VExterior from '../../images/vicino/Leasing-office-exterior.jpg';
 
 export default class Exterior extends Component {
   render() {
@@ -9,7 +14,54 @@ export default class Exterior extends Component {
       <div className="Property">
         <h4>Exterior</h4>
         <Carousel interval="null" className="Carousel">
-          
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={P5Exterior}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={VExterior}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={LexEntrance}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={LexMainExterior}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="Carousel-image"
+              src={LexExterior}
+              alt=""
+            />
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
         </Carousel>
       </div>
     )
