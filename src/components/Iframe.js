1a6822258cd7b0ce45fae4aa5504203e682dc1d8
bbennett7 <bryn.bennett@gmail.com>
2019-10-11 16:20:31 -0700

    Remove uneccesary imports

--- src/components/Iframe.js
+++ src/components/Iframe.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Iframe extends Component {
   render() {
     return(
-      <iframe className="Iframe" src={this.props.src} width={this.props.width} height={this.props.height} />
+      <iframe className="Iframe" title="Map" src={this.props.src} width={this.props.width} height={this.props.height} />
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Render } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
 import './index.css';
 import App from './App';
