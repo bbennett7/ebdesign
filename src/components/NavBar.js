63cbf85b8d6b85dd9c7393d475126255315a2510
bbennett7 <bryn.bennett@gmail.com>
2019-10-10 20:51:59 -0700

    Restyle nav dropdowns

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,55 +3,53 @@ import '../App.css'
 import { Link } from 'react-router-dom';
 import { Nav, NavDropdown } from 'react-bootstrap';
 
-import NavButton from './NavButton'
-
 export default class NavBar extends Component {
   render() {
     return (
-      <Nav fill className="Nav-bar">
+      <Nav fill>
         <Nav.Item>
-          <Nav.Link className="nav-border nav-item"><Link to="/home"> Home </Link></Nav.Link>
+          <Link to="/home"> Home </Link>
         </Nav.Item>
         <Nav.Item>
-          <NavDropdown title="Multi-family" className="Top-level nav-dropdown nav-border">
-            <NavDropdown drop="right" variant="secondary" title="By Property" className="Second-level nav-dropdown">
-               <NavDropdown.Item className="Inner-drop" eventKey="1.1"> <Link to="/multi-family/Bayshore"> Bayshore </Link> </NavDropdown.Item>
-               <NavDropdown.Item className="Inner-drop" eventKey="1.2"> <Link to="/multi-family/The-Chadwick"> Chadwick </Link> </NavDropdown.Item>
-               <NavDropdown.Item className="Inner-drop" eventKey="1.3"> <Link to="/multi-family/Corsica"> Corsica </Link> </NavDropdown.Item>
-               <NavDropdown.Item className="Inner-drop" eventKey="1.4"><Link to="/multi-family/Harborview"> Harborview </Link></NavDropdown.Item>
-               <NavDropdown.Item className="Inner-drop" eventKey="1.5"> <Link to="/multi-family/The-Lexington"> The Lexington </Link></NavDropdown.Item>
-               <NavDropdown.Item className="Inner-drop" eventKey="1.6"> <Link to="/multi-family/Parc-5"> Parc @ 5 </Link> </NavDropdown.Item>
-               <NavDropdown.Item className="Inner-drop" eventKey="1.7"> <Link to="/multi-family/Rockwood"> Rockwood </Link> </NavDropdown.Item>
-               <NavDropdown.Item className="Inner-drop" eventKey="1.8"> <Link to="/multi-family/Vicino"> Vicino </Link> </NavDropdown.Item>
+          <NavDropdown title="Multi-family" className="Top-level">
+            <NavDropdown drop="right" variant="secondary" title="By Property" className="Second-level">
+              <Link className="Bottom-level" to="/multi-family/Bayshore"> Bayshore </Link>
+              <Link className="Bottom-level" to="/multi-family/The-Chadwick"> Chadwick </Link>
+              <Link className="Bottom-level" to="/multi-family/Corsica"> Corsica </Link>
+              <Link className="Bottom-level" to="/multi-family/Harborview"> Harborview </Link>
+              <Link className="Bottom-level" to="/multi-family/The-Lexington"> The Lexington </Link>
+              <Link className="Bottom-level" to="/multi-family/Parc-5"> Parc @ 5 </Link>
+              <Link className="Bottom-level" to="/multi-family/Rockwood"> Rockwood </Link>
+              <Link className="Bottom-level" to="/multi-family/Vicino"> Vicino </Link>
              </NavDropdown>
-             <NavDropdown drop="right" variant="secondary" title="By Area" className="Second-level nav-dropdown">
-                <NavDropdown.Item className="Inner-drop" eventKey="2.1"><Link to="/multi-family/barbecue"> Barbecue </Link></NavDropdown.Item>
-                <NavDropdown.Item className="Inner-drop" eventKey="2.5"><Link to="/multi-family/club-room"> Club Room </Link></NavDropdown.Item>
-                <NavDropdown.Item className="Inner-drop" eventKey="2.2"><Link to="/multi-family/exterior"> Exterior </Link></NavDropdown.Item>
-                <NavDropdown.Item className="Inner-drop" eventKey="2.3"><Link to="/multi-family/fitness-center"> Fitness Center </Link></NavDropdown.Item>
-                <NavDropdown.Item className="Inner-drop" eventKey="2.4"><Link to="/multi-family/game-room"> Game Room </Link></NavDropdown.Item>
-                <NavDropdown.Item className="Inner-drop" eventKey="2.5"><Link to="/multi-family/leasing-office"> Leasing Office </Link></NavDropdown.Item>
-                <NavDropdown.Item className="Inner-drop" eventKey="2.6"><Link to="/multi-family/outdoor-lounge"> Outdoor Lounge </Link></NavDropdown.Item>
-                <NavDropdown.Item className="Inner-drop" eventKey="2.7"><Link to="/multi-family/pool"> Pool </Link></NavDropdown.Item>
+             <NavDropdown drop="right" variant="secondary" title="By Area" className="Second-level">
+                <Link className="Bottom-level" to="/multi-family/barbecue"> Barbecue </Link>
+                <Link className="Bottom-level" to="/multi-family/club-room"> Club Room </Link>
+                <Link className="Bottom-level" to="/multi-family/exterior"> Exterior </Link>
+                <Link className="Bottom-level" to="/multi-family/fitness-center"> Fitness Center </Link>
+                <Link className="Bottom-level" to="/multi-family/game-room"> Game Room </Link>
+                <Link className="Bottom-level" to="/multi-family/leasing-office"> Leasing Office </Link>
+                <Link className="Bottom-level" to="/multi-family/outdoor-lounge"> Outdoor Lounge </Link>
+                <Link className="Bottom-level" to="/multi-family/pool"> Pool </Link>
               </NavDropdown>
            </NavDropdown>
         </Nav.Item>
         <Nav.Item>
-          <NavDropdown title="Residential" className="Top-level nav-dropdown nav-border">
-            <NavDropdown.Item eventKey="3.1"><Link to="/residential/backyard"> Backyard </Link> </NavDropdown.Item>
-            <NavDropdown.Item eventKey="3.2"> <Link to="/residential/bathroom"> Bathroom </Link> </NavDropdown.Item>
-            <NavDropdown.Item eventKey="3.3"> <Link to="/residential/kitchen"> Kitchen </Link> </NavDropdown.Item>
-            <NavDropdown.Item eventKey="3.4"> <Link to="/residential/living-room"> Living Room </Link> </NavDropdown.Item>
+          <NavDropdown title="Residential" className="Top-level">
+            <Link className="Bottom-level" to="/residential/backyard"> Backyard </Link>
+            <Link className="Bottom-level" to="/residential/bathroom"> Bathroom </Link>
+            <Link className="Bottom-level" to="/residential/kitchen"> Kitchen </Link>
+            <Link className="Bottom-level" to="/residential/living-room"> Living Room </Link>
           </NavDropdown>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link className="nav-border nav-item"> <Link to="/services"> Services </Link> </Nav.Link>
+          <Link to="/services"> Services </Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link className="nav-border nav-item"> <Link to="/about"> About </Link> </Nav.Link>
+          <Link to="/about"> About </Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link className="nav-border nav-item" href="/contact"><Link to="/contact"> Contact </Link></Nav.Link>
+          <Link to="/contact"> Contact </Link>
         </Nav.Item>
       </Nav>
     )
