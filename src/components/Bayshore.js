fe175037b3e2f57ef39cdf8c20435a32bb887522
bbennett7 <bryn.bennett@gmail.com>
2019-10-19 21:17:27 -0700

    Move bayshore images

--- src/components/Bayshore.js
+++ src/components/Bayshore.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+// import Bench from '../images/bayshore/Bench.jpg';
+// import Chairs from '../images/bayshore/Chairs.jpg';
+// import CommonArea from '../images/bayshore/Common-area.jpg';
+// import Desk from '../images/bayshore/Desk.jpg';
+// import Kitchen from '../images/bayshore/Kitchen.jpg';
 
-import Bench from '../images/bayshore/Bench.jpg';
-import Chairs from '../images/bayshore/Chairs.jpg';
-import CommonArea from '../images/bayshore/Common-area.jpg';
-import Desk from '../images/bayshore/Desk.jpg';
-import Kitchen from '../images/bayshore/Kitchen.jpg';
 
 export default class Bayshore extends Component {
   render() {
@@ -15,51 +15,31 @@ export default class Bayshore extends Component {
           <h4>Bayshore</h4>
           <Carousel className="Carousel">
             <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={CommonArea}
-                alt=""
-              />
+              <img className="Carousel-image" src="https://i.ibb.co/z6cWprb/Common-area.jpg" alt="Common-area" border="0" />
               <Carousel.Caption>
               </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={Kitchen}
-                alt=""
-              />
+              <img className="Carousel-image" src="https://i.ibb.co/DDG8J2W/Kitchen.jpg" alt="Kitchen" border="0" />
               <Carousel.Caption>
               </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={Chairs}
-                alt=""
-              />
+              <img className="Carousel-image" src="https://i.ibb.co/VjrsYr5/Chairs.jpg" alt="Chairs" border="0" />
               <Carousel.Caption>
               </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={Bench}
-                alt=""
-              />
+              <img className="Carousel-image" src="https://i.ibb.co/86cqTMC/Bench.jpg" alt="Bench" border="0" />
               <Carousel.Caption>
               </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={Desk}
-                alt=""
-              />
+              <img className="Carousel-image" src="https://i.ibb.co/vZ89nmf/Desk.jpg" alt="Desk" border="0" />
               <Carousel.Caption>
               </Carousel.Caption>
             </Carousel.Item>
--- src/components/Chadwick.js
+++ src/components/Chadwick.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
-import BBQs from '../images/chadwick/BBQs.jpg';
-import Pool from '../images/chadwick/Pool.jpg';
-import Seating from '../images/chadwick/Seating.jpg';
-import Fireplace from '../images/chadwick/Fireplace.png';
-import GameRoom from '../images/chadwick/Game-room.jpg';
-import LeasingOffice from '../images/chadwick/Leasing-office.jpg';
-import PoolSeating from '../images/chadwick/Pool-seating.png';
+// import BBQs from '../images/chadwick/BBQs.jpg';
+// import Pool from '../images/chadwick/Pool.jpg';
+// import Seating from '../images/chadwick/Seating.jpg';
+// import Fireplace from '../images/chadwick/Fireplace.png';
+// import GameRoom from '../images/chadwick/Game-room.jpg';
+// import LeasingOffice from '../images/chadwick/Leasing-office.jpg';
+// import PoolSeating from '../images/chadwick/Pool-seating.png';
+
+
+
+
+
 
 export default class Chadwick extends Component {
   render() {
