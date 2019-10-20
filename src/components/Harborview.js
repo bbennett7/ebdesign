75f64aba271748dae3741b31f3492613a59a3da3
bbennett7 <bryn.bennett@gmail.com>
2019-10-19 21:40:35 -0700

    remove harborview images

--- src/components/Harborview.js
+++ src/components/Harborview.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
-
-
 export default class Harborview extends Component {
   render() {
     return(
@@ -11,51 +9,27 @@ export default class Harborview extends Component {
           <h4>Harborview</h4>
           <Carousel className="Carousel">
             <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={Exterior}
-                alt=""
-              />
+              <img className="Carousel-image" src="https://i.ibb.co/PN92nmv/Exterior.jpg" alt="Exterior" border="0" />
             </Carousel.Item>
 
             <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={LeasingOffice}
-                alt=""
-              />
+              <img className="Carousel-image" src="https://i.ibb.co/Tm4JF1c/Leasing-office.jpg" alt="Leasing-office" border="0" />
             </Carousel.Item>
 
             <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={LeasingOffice2}
-                alt=""
-              />
+              <img className="Carousel-image" src="https://i.ibb.co/4P9yLBd/Leasing-office2.jpg" alt="Leasing-office2" border="0" />
+            </Carousel.Item>
+
+              <Carousel.Item>
+              <img className="Carousel-image" src="https://i.ibb.co/d61wC7v/Fitness-center.jpg" alt="Fitness-center" border="0" />
             </Carousel.Item>
 
             <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={FitnessCenter}
-                alt=""
-              />
+              <img className="Carousel-image" src="https://i.ibb.co/VmMT0fZ/Pool.jpg" alt="Pool" border="0" />
             </Carousel.Item>
 
             <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={Pool}
-                alt=""
-              />
-            </Carousel.Item>
-
-            <Carousel.Item>
-              <img
-                className="Carousel-image"
-                src={Barbecue}
-                alt=""
-              />
+              <img className="Carousel-image" src="https://i.ibb.co/c3S5qCh/Barbecue.jpg" alt="Barbecue" border="0" />
             </Carousel.Item>
           </Carousel>
         </div>
