e7cfc5c854194c9ba2b085864bfe4b35d5b9007a
bbennett7 <bryn.bennett@gmail.com>
2019-10-27 13:51:28 -0700

    Add Waterscape

--- src/components/Waterscape.js
+++ src/components/Waterscape.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
-import LeasingOffice from '../images/rockwood/Leasing-office.jpg';
+import W1 from '../images/waterscape/Waterscape1.jpg';
+import W2 from '../images/waterscape/Waterscape2.jpg';
+import W3 from '../images/waterscape/Waterscape3.jpg';
+import W4 from '../images/waterscape/Waterscape4.jpg';
 
 export default class Waterscape extends Component {
   render() {
@@ -9,12 +12,35 @@ export default class Waterscape extends Component {
       <div className="Property-grid">
         <div className="Property">
           <h4>Waterscape</h4>
-          <h1>ADD IMAGES</h1>
           <Carousel className="Carousel">
             <Carousel.Item>
               <img
                 className="Carousel-image"
-                src={LeasingOffice}
+                src={W3}
+                alt=""
+              />
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+                className="Carousel-image"
+                src={W4}
+                alt=""
+              />
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+                className="Carousel-image"
+                src={W2}
+                alt=""
+              />
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+                className="Carousel-image"
+                src={W1}
                 alt=""
               />
             </Carousel.Item>
